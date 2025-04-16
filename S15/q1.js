072e6e28cd9f10974ccacde3638b594ca8b217ea
AmolMahale96K <149690473+AmolMahale96K@users.noreply.github.com>
2025-04-16 13:15:30 +0530

    Update q1.js

--- S15/q1.js
+++ S15/q1.js
@@ -1,35 +1,20 @@
-// Import the MySQL module
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-// Create a connection to the MySQL database
 const connection = mysql.createConnection({
-  host: 'localhost',       // Hostname where the MySQL server is running
-  user: 'root',            // MySQL username
-  password: '',            // MySQL password
-  database: 'your_database_name'  // The name of the database
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'college',
+  port: 3306
 });
 
-// Connect to the MySQL server
 connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.stack);
-    return;
-  }
-  console.log('Connected as id ' + connection.threadId);
+  if (err) throw err;
+  console.log('Connected');
+
+  connection.query('SELECT * FROM students', (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    connection.end();
+  });
 });
-
-// Query to select all records from the "students" table
-const query = 'SELECT * FROM students';
-
-// Execute the query
-connection.query(query, (err, results) => {
-  if (err) {
-    console.error('Error fetching data: ' + err.stack);
-    return;
-  }
-  // Display the results in the console
-  console.log('Students Data:', results);
-});
-
-// Close the connection
-connection.end();
