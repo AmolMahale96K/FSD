ef5538b1deab7fc8b37408648745d840dd48adca
AmolMahale96K <149690473+AmolMahale96K@users.noreply.github.com>
2025-04-16 13:50:42 +0530

    Update q1.js

--- S3/q1.js
+++ S3/q1.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-// MySQL connection
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -13,44 +12,30 @@ const db = mysql.createConnection({
 });
 
 db.connect((err) => {
-  // if (err) throw err;
+  if (err) throw err;
   console.log('MySQL Connected...');
 });
 
-
-// Register
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
   db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err) => {
-    if (err)
-       return res.send('<h3 style="color:red;">❌ Error while registering user!</h3>');
-    res.send('<h3 style="color:green;">✅ Registered successfully!</h3>');
+    if (err) return res.send('Error while registering user.');
+    res.send('Registered successfully.');
   });
 });
 
-// Login
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
-    if (err)
-       return res.send('<h3 style="color:red;">❌ Login error!</h3>');
+    if (err) return res.send('Login error.');
     if (results.length > 0) {
-      res.send('<h3 style="color:green;">✅ Login successful!</h3>');
+      res.send('Login successful.');
     } else {
-      res.send('<h3 style="color:red;">❌ Invalid credentials!</h3>');
+      res.send('Invalid credentials.');
     }
   });
 });
 
-// Start server
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
-
-
-
-
-
-// Create table if not exists
-
-  
